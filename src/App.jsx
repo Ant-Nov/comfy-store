0c3c9a7aba414089c38ad72d6aeb9ba5d4afecf5
Anton Novikov <anton1995novikov@gmail.com>
2026-05-23 14:10:50 +0300

    added basename to router

--- src/App.jsx
+++ src/App.jsx
@@ -28,63 +28,66 @@ import { store } from './store';
 
 const queryClient = new QueryClient();
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: Homelayout,
-    errorElement: <Error />,
-    children: [
-      {
-        index: true,
-        errorElement: <SinglePageError />,
-        loader: landingLoader(queryClient),
-        Component: Landing,
-      },
-      {
-        path: 'about',
-        Component: About,
-      },
-      {
-        path: 'cart',
-        Component: Cart,
-      },
-      {
-        path: 'products',
-        errorElement: <SinglePageError/>,
-        loader: allProductsLoader(queryClient),
-        Component: Products,
-      },
-      {
-        path: 'products/:id',
-        errorElement: <SinglePageError/>,
-        loader: productLoader(queryClient),
-        Component: SingleProduct,
-      },
-      {
-        path: 'checkout',
-        errorElement: <SinglePageError/>,
-        loader: checkoutLoader(store),
-        Component: Checkout,
-      },
-      {
-        path: 'orders',
-        errorElement: <SinglePageError/>,
-        loader: ordersLoader(store, queryClient),
-        Component: Orders,
-      },
-    ]
-  },
-  {
-    path: 'login',
-    errorElement: Error,
-    Component: Login,
-  },
-  {
-    path: 'register',
-    errorElement: Error,
-    Component: Register,
-  }
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      Component: Homelayout,
+      errorElement: <Error />,
+      children: [
+        {
+          index: true,
+          errorElement: <SinglePageError />,
+          loader: landingLoader(queryClient),
+          Component: Landing,
+        },
+        {
+          path: 'about',
+          Component: About,
+        },
+        {
+          path: 'cart',
+          Component: Cart,
+        },
+        {
+          path: 'products',
+          errorElement: <SinglePageError/>,
+          loader: allProductsLoader(queryClient),
+          Component: Products,
+        },
+        {
+          path: 'products/:id',
+          errorElement: <SinglePageError/>,
+          loader: productLoader(queryClient),
+          Component: SingleProduct,
+        },
+        {
+          path: 'checkout',
+          errorElement: <SinglePageError/>,
+          loader: checkoutLoader(store),
+          Component: Checkout,
+        },
+        {
+          path: 'orders',
+          errorElement: <SinglePageError/>,
+          loader: ordersLoader(store, queryClient),
+          Component: Orders,
+        },
+      ]
+    },
+    {
+      path: 'login',
+      errorElement: Error,
+      Component: Login,
+    },
+    {
+      path: 'register',
+      errorElement: Error,
+      Component: Register,
+    }
+  ],
+  { basename: '/comfy-store/', }
+);
 
 function App() {
   return (
